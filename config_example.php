4a2b4151d1bbb0d8c0e7374b7e3e53bb093772a0
Krzysztof Przybylski <krzysiek7775@gmail.com>
2019-01-10 19:59:52 +0100

    Add .gitignore Add config_example Add db_test

---
+++ config_example.php
@@ -0,0 +1,12 @@
+<?php
+
+// Make your 'config.php' file based on this
+
+$db_address = '';
+$db_port = '';
+
+return array(
+    'db_connection' => "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = " . $db_address . ")(PORT = " . $db_port . ")))(CONNECT_DATA=(SID=orcl)))",
+    'db_user' => '',
+    'db_password' => ''
+);
